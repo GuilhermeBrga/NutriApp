0512f00cfd7f9496f78c7ae2bdd1cfaeb792de59
João Guilherme Braga Nascimento <137304292+GuilhermeBrga@users.noreply.github.com>
2025-05-19 10:24:16 -0300

    Update handler.clj

--- api_nutri_app/src/api_nutri_app/handler.clj
+++ api_nutri_app/src/api_nutri_app/handler.clj
@@ -1,11 +1,62 @@
+; HANDLER PURO:
+
+;(ns api-nutri-app.handler
+;  (:require [compojure.core :refer :all]
+;            [compojure.route :as route]
+;            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+;
+;(defroutes app-routes
+;           (GET "/" [] "Hello World")
+;           (route/not-found "Not Found"))
+;
+;(def app
+;  (wrap-defaults app-routes site-defaults))
+;
+; DEPENDENCIAS PURAS:
+;
+; :dependencies [[org.clojure/clojure "1.10.0"]
+;               [compojure "1.6.1"]
+;               [ring/ring-defaults "0.3.2"]]
+
+
+; =====================================================================================================================
+
+
+;; HANDLER COM API USDA (GOVERNO AMERICANO)
 (ns api-nutri-app.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clj-http.client :as http]
+            [cheshire.core :as json]
+            [environ.core :refer [env]]))
+
+(def api-key (env :usda-api-key))
+
+(defn buscar-usda [alimento]
+  (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
+        
+        params {:query alimento
+                :pageSize 1
+                :api_key api-key}
+        
+        response (http/get url {:query-params params :as :json})
+        
+        items (get-in response [:body :foods])]
+    
+    (mapv (fn [item]
+            {:descricao (:description item)
+             :gramas    (:servingSize item)
+             :energia-kcal (get-in item [:foodNutrients 0 :value])})
+          items)))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+           (GET "/" [] "Hello World")
+           (GET "/usda" [alimento]
+             {:status 200
+              :headers {"Content-Type" "application/json"}
+              :body (json/encode (buscar-usda alimento))})
+           (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
