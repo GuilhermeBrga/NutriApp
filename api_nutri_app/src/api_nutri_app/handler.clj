020271da4ca3dd409e1d7af1898be7107e9e50b9
João Guilherme Braga Nascimento <137304292+GuilhermeBrga@users.noreply.github.com>
2025-05-27 08:12:43 -0300

    Atualização conexão front/back handler.clj

--- api_nutri_app/src/api_nutri_app/handler.clj
+++ api_nutri_app/src/api_nutri_app/handler.clj
@@ -1,5 +1,3 @@
-;; HANDLER COM API USDA (GOVERNO AMERICANO)
-
 (ns api-nutri-app.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
@@ -8,19 +6,24 @@
             [cheshire.core :as json]
             [environ.core :refer [env]]))
 
+(def dados_user (atom []))
+(def alimentos_armazenados (atom []))
+(def atividades_armazenadas (atom []))
+
 (def api-key (env :usda-api-key))
 
 (defn buscar-usda [alimento]
+
   (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
-        
+
         params {:query alimento
                 :pageSize 1
                 :api_key api-key}
-        
+
         response (http/get url {:query-params params :as :json})
-        
+
         items (get-in response [:body :foods])]
-    
+
     (mapv (fn [item]
             {:descricao (:description item)
              :gramas    (:servingSize item)
@@ -28,12 +31,55 @@
           items)))
 
 (defroutes app-routes
-           (GET "/" [] "Hello World")
+
+           (GET "/" [] "NutriApp API Online!")
+
            (GET "/usda" [alimento]
              {:status 200
               :headers {"Content-Type" "application/json"}
               :body (json/encode (buscar-usda alimento))})
+
+           (POST "/registro/user" req
+
+             (let [dados (json/decode (slurp (:body req)) true)]
+
+               (swap! dados_user conj dados)
+
+               (println "Dados de usuário recebidos:" dados)
+
+               {:status 200 :body "Dados registrados!"}))
+
+           (POST "/registro/alimento" req
+
+             (let [dados (json/decode (slurp (:body req)) true)]
+
+               (swap! alimentos_armazenados conj dados)
+
+               (println "Alimento recebido:" dados)
+
+               {:status 200 :body "Alimento registrado!"}))
+
+           (POST "/registro/atividade" req
+
+             (let [dados (json/decode (slurp (:body req)) true)]
+
+               (swap! atividades_armazenadas conj dados)
+
+               (println "Atividade recebida:" dados)
+
+               {:status 200 :body "Atividade registrada!"}))
+
+           (GET "/dados" []
+
+             {:status 200
+
+              :headers {"Content-Type" "application/json"}
+
+              :body (json/encode {:alimentos @alimentos_armazenados
+                                  :atividades @atividades_armazenadas
+                                  :dados @dados_user})})
+
            (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes (assoc site-defaults :security {:anti-forgery false})))
