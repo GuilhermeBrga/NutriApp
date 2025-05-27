674ed3b85db762fc1614ae9290592f0eff00e2a5
arthurpcidrao <arthurpcidrao@gmail.com>
2025-05-27 10:02:54 -0300

    regra de 3 para gramas do usuario

--- api_nutri_app/src/api_nutri_app/handler.clj
+++ api_nutri_app/src/api_nutri_app/handler.clj
@@ -1,18 +1,3 @@
-;; HANDLER INICIAL
-;; (ns api-nutri-app.handler
-;;   (:require [compojure.core :refer :all]
-;;             [compojure.route :as route]
-;;             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
-
-;; (defroutes app-routes
-;;   (GET "/" [] "Hello World")
-;;   (route/not-found "Not Found"))
-
-;; (def app
-;;   (wrap-defaults app-routes site-defaults))
-
-
-
 ;; HANDLER COM API USDA (GOVERNO AMERICANO)
 (ns api-nutri-app.handler
   (:require [compojure.core :refer :all]
@@ -20,75 +5,116 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [clj-http.client :as http]
             [cheshire.core :as json]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]])
+)
+
+(def dados_user (atom []))
+(def alimentos_armazenados (atom []))
+(def atividades_armazenadas (atom []))
 
 (def api-key (env :usda-api-key))
 
-(defn buscar-usda [alimento]
+;; (def libretranslate-url "https://libretranslate.de/translate")
+
+;; (defn buscar-usda [alimento]
+;;   (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
+;;         params {:query alimento ; alimento pesquisado, ex.: "banana"
+;;                 :pageSize 10 ; quantos alimentos diferentes eu quero que retorne
+;;                 :api_key api-key} ; chave api
+;;         response (http/get url {:query-params params :as :json})
+;;         items (get-in response [:body :foods])]
+;;     (mapv (fn [item]
+;;             {:descricao (:description item)
+;;              :gramas    (:servingSize item)
+;;              :energia-kcal (get-in item [:foodNutrients 0 :value])})
+;;           items)))
+
+
+(defn kcal-ajustado [gramas-usuario gramas-usda kcal-usda]
+  (/ (* gramas-usuario kcal-usda) gramas-usda) ;; regra de 3
+)
+
+
+;; (defn traduzir
+;;   [texto origem destino]
+;;   (let [response (http/post libretranslate-url
+;;                             {:headers {"Content-Type" "application/json"}
+;;                              :body (json/encode {:q texto
+;;                                                  :source origem
+;;                                                  :target destino
+;;                                                  :format "text"})
+;;                              :as :json})]
+;;     (get-in response [:body :translatedText])))
+
+
+
+(defn buscar-usda [alimento gramas-usuario]
   (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
-        params {:query alimento
-                :pageSize 5
-                :api_key api-key}
+        params {:query alimento ; alimento pesquisado, ex.: "banana"
+                :pageSize 1 ; quantos alimentos diferentes eu quero que retorne
+                :api_key api-key} ; chave api
         response (http/get url {:query-params params :as :json})
         items (get-in response [:body :foods])]
     (mapv (fn [item]
             {:descricao (:description item)
-             :gramas    (:servingSize item)
-             :energia-kcal (get-in item [:foodNutrients 0 :value])})
-          items)))
+             :gramas    gramas-usuario ;(:servingSize item)
+             :energia-kcal (if (not (nil? (:servingSize item))) 
+             (kcal-ajustado gramas-usuario (:servingSize item) (get-in item [:foodNutrients 0 :value])) 
+             (kcal-ajustado gramas-usuario 100 (get-in item [:foodNutrients 0 :value])))
+             }) items)
+    )
+)
+;; testar com seaweed (alga), pq não ta dando certo
+
+
+(defn busca-resultados [alimento]
+  ; definir como que eu vou pegar a variável gramas-usuario (substituirá o 200 abaixo)
+  (buscar-usda alimento 200)
+)
+
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  
+  (GET "/" [] "Nutri App by Arthur e Guilherme")
+ 
   (GET "/usda" [alimento]
        {:status 200
         :headers {"Content-Type" "application/json"}
-        :body (json/encode (buscar-usda alimento))})
-  (route/not-found "Not Found"))
+        :body (json/encode (busca-resultados alimento))}
+  )
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+  ;; (GET "/traduzir" [texto origem destino]
+  ;;      (let [traducao (traduzir texto origem destino)]
+  ;;        {:status 200
+  ;;         :headers {"Content-Type" "application/json"}
+  ;;         :body (json/encode {:traduzido traducao})})
+  ;; )
+  
+  (route/not-found "Not Found")
 
-
-
-;; HANDLER COM API OPENFOODSFACTS (TEM EM PORTUGUES)
-;; (ns api-nutri-app.handler
-;;   (:require [compojure.core :refer :all]
-;;             [compojure.route :as route]
-;;             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-;;             [clj-http.client :as http]
-;;             [cheshire.core :as json]))
-
-;; (defn buscar-openfoodfacts [nome]
-;;   (let [url "https://world.openfoodfacts.org/cgi/search.pl"
-;;         params {:search_terms nome
-;;                 :search_simple 1
-;;                 :action "process"
-;;                 :json 1
-;;                 :lang "pt"}
-;;         response (http/get url {:query-params params :as :json})
-;;         produtos (get-in response [:body :products])]
-;;     (->> produtos
-;;          (filter #(or (:product_name_pt %) (:product_name %)))
-;;          (map (fn [p]
-;;                 {:nome         (or (:product_name_pt p) (:product_name p))
-;;                  :marca        (:brands p)
-;;                  :quantidade   (:quantity p)
-;;                  :porcao       (:serving_size p)
-;;                  :imagem       (:image_url p) ; <-- aqui está a imagem!
-;;                  :energia-kcal (get-in p [:nutriments :energy-kcal_100g])
-;;                  :proteinas    (get-in p [:nutriments :proteins_100g])
-;;                  :carboidratos (get-in p [:nutriments :carbohydrates_100g])
-;;                  :gorduras     (get-in p [:nutriments :fat_100g])}))
-;;          (take 5))))
-
-;; (defroutes app-routes
-;;   (GET "/" [] "Hello World")
-;;   (GET "/alimento" [nome]
-;;        {:status 200
-;;         :headers {"Content-Type" "application/json"}
-;;         :body (json/encode (buscar-openfoodfacts nome))})
-;;   (route/not-found "Not Found"))
+)
 
 ;; (def app
-;;   (wrap-defaults app-routes site-defaults))
+;;   (wrap-defaults app-routes site-defaults)
+;; )
 
+
+(def app
+  (wrap-defaults app-routes
+                 (assoc-in site-defaults [:security :anti-forgery] false)))
+
+
+;; fazer tratamento de visualização:
+;; se gramas = null e energia-kcal maior que zero
+;; então considere gramas = 100g (deu bom pra um exemplo, deve dar pra tudim)
+
+;; se gramas e energia-kcal são nulos, não exiba esse resultado
+
+
+; exemplo
+; 100g --- 450kcal
+; 210g --- x kcal
+
+; x = (210 * 450)/100
+
+; usar o LibreTranslate -> verificar como funciona para configurar
