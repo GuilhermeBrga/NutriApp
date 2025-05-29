1367b5d489df19973826ac33defbff68e878392f
arthurpcidrao <arthurpcidrao@gmail.com>
2025-05-29 10:51:10 -0300

    integracao com tradutor

--- api_nutri_app/src/api_nutri_app/handler.clj
+++ api_nutri_app/src/api_nutri_app/handler.clj
@@ -14,107 +14,123 @@
 
 (def api-key (env :usda-api-key))
 
-;; (def libretranslate-url "https://libretranslate.de/translate")
 
-;; (defn buscar-usda [alimento]
-;;   (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
-;;         params {:query alimento ; alimento pesquisado, ex.: "banana"
-;;                 :pageSize 10 ; quantos alimentos diferentes eu quero que retorne
-;;                 :api_key api-key} ; chave api
-;;         response (http/get url {:query-params params :as :json})
-;;         items (get-in response [:body :foods])]
-;;     (mapv (fn [item]
-;;             {:descricao (:description item)
-;;              :gramas    (:servingSize item)
-;;              :energia-kcal (get-in item [:foodNutrients 0 :value])})
-;;           items)))
-
-
-(defn kcal-ajustado [gramas-usuario gramas-usda kcal-usda]
-  (/ (* gramas-usuario kcal-usda) gramas-usda) ;; regra de 3
+(defn traduzir-alimento [texto de para]
+  (let [url "http://localhost:5000/translate"
+        body {:q texto
+              :source de
+              :target para
+              :format "text"}
+        response (http/post url {:body (json/encode body)
+                                 :headers {"Content-Type" "application/json"}
+                                 :as :json})]
+    (:translatedText (:body response)))
 )
 
 
-;; (defn traduzir
-;;   [texto origem destino]
-;;   (let [response (http/post libretranslate-url
-;;                             {:headers {"Content-Type" "application/json"}
-;;                              :body (json/encode {:q texto
-;;                                                  :source origem
-;;                                                  :target destino
-;;                                                  :format "text"})
-;;                              :as :json})]
-;;     (get-in response [:body :translatedText])))
+(defn kcal-ajustado [gramas-usuario kcal-usda]
+  (/ (* gramas-usuario kcal-usda) 100)
+)
 
 
-
-(defn buscar-usda [alimento gramas-usuario]
+(defn buscar-usda [alimento]
   (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
-        params {:query alimento ; alimento pesquisado, ex.: "banana"
-                :pageSize 1 ; quantos alimentos diferentes eu quero que retorne
-                :api_key api-key} ; chave api
+        params {:query alimento
+                :dataType ["Survey (FNDDS)"]
+                :pageSize 10
+                :api_key api-key}
         response (http/get url {:query-params params :as :json})
         items (get-in response [:body :foods])]
     (mapv (fn [item]
-            {:descricao (:description item)
-             :gramas    gramas-usuario ;(:servingSize item)
-             :energia-kcal (if (not (nil? (:servingSize item))) 
-             (kcal-ajustado gramas-usuario (:servingSize item) (get-in item [:foodNutrients 0 :value])) 
-             (kcal-ajustado gramas-usuario 100 (get-in item [:foodNutrients 0 :value])))
-             }) items)
-    )
+            (let [energia (some #(when (= "Energy" (:nutrientName %))
+                                   (:value %))
+                                (:foodNutrients item))
+                  descricao-pt (traduzir-alimento (:description item) "en" "pt")]
+              {:descricao descricao-pt
+               :energia-kcal energia}))
+          items))
 )
-;; testar com seaweed (alga), pq não ta dando certo
 
 
-(defn busca-resultados [alimento]
-  ; definir como que eu vou pegar a variável gramas-usuario (substituirá o 200 abaixo)
-  (buscar-usda alimento 200)
+
+(defn buscar-resultados [alimento]
+  (buscar-usda (traduzir-alimento alimento "pt" "en"))
 )
 
 
 (defroutes app-routes
   
-  (GET "/" [] "Nutri App by Arthur e Guilherme")
+  (GET "/" [] "Nutri App by Arthur & Guilherme")
  
+
   (GET "/usda" [alimento]
        {:status 200
         :headers {"Content-Type" "application/json"}
-        :body (json/encode (busca-resultados alimento))}
+        :body (json/encode (buscar-resultados alimento))}
   )
 
-  ;; (GET "/traduzir" [texto origem destino]
-  ;;      (let [traducao (traduzir texto origem destino)]
-  ;;        {:status 200
-  ;;         :headers {"Content-Type" "application/json"}
-  ;;         :body (json/encode {:traduzido traducao})})
-  ;; )
+
+  (GET "/contem/user" []
+
+             (if (empty? @dados_user)
+
+               {:status 200
+                :headers {"Content-Type" "application/json"}
+                :body (json/encode {:vazio? true})}
+
+               {:status 200
+                :headers {"Content-Type" "application/json"}
+                :body (json/encode {:vazio? false})})
+  )
+
+
+  (POST "/traduzir" request
+      (let [params (json/decode (slurp (:body request)) true)
+            texto (:texto params)
+            de    (:de params)
+            para  (:para params)]
+        {:status 200
+         :headers {"Content-Type" "application/json"}
+         :body (json/encode (traduzir-alimento texto de para))})
+  )
+
+
+  (POST "/registro/user" req
+
+    (let [dados (json/decode (slurp (:body req)) true)]
+      (swap! dados_user conj dados)
+      (println "Dados de usuário recebidos:" dados)
+      {:status 200 :body "Dados registrados!"})
+  )
+
+  (POST "/registro/alimento" req
+    (let [dados (json/decode (slurp (:body req)) true)]
+      (swap! alimentos_armazenados conj dados)
+      (println "Alimento recebido:" dados)
+      {:status 200 :body "Alimento registrado!"})
+  )
+
+  (POST "/registro/atividade" req
+    (let [dados (json/decode (slurp (:body req)) true)]
+      (swap! atividades_armazenadas conj dados)
+      (println "Atividade recebida:" dados)
+      {:status 200 :body "Atividade registrada!"})
+  )
+
+  (GET "/dados" []
+    {:status 200
+    :headers {"Content-Type" "application/json"}
+    :body (json/encode {:alimentos @alimentos_armazenados
+                        :atividades @atividades_armazenadas
+                        :dados @dados_user})}
+  )
   
+
   (route/not-found "Not Found")
 
 )
 
-;; (def app
-;;   (wrap-defaults app-routes site-defaults)
-;; )
-
-
 (def app
-  (wrap-defaults app-routes
-                 (assoc-in site-defaults [:security :anti-forgery] false)))
+  (wrap-defaults app-routes site-defaults)
+)
 
-
-;; fazer tratamento de visualização:
-;; se gramas = null e energia-kcal maior que zero
-;; então considere gramas = 100g (deu bom pra um exemplo, deve dar pra tudim)
-
-;; se gramas e energia-kcal são nulos, não exiba esse resultado
-
-
-; exemplo
-; 100g --- 450kcal
-; 210g --- x kcal
-
-; x = (210 * 450)/100
-
-; usar o LibreTranslate -> verificar como funciona para configurar
