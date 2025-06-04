37f945e99792db52e0b72ccc21a503a4c2af07f7
João Guilherme Braga Nascimento <137304292+GuilhermeBrga@users.noreply.github.com>
2025-06-04 17:37:27 -0300

    Update handler.clj

--- api_nutri_app/src/api_nutri_app/handler.clj
+++ api_nutri_app/src/api_nutri_app/handler.clj
@@ -7,13 +7,18 @@
             [environ.core :refer [env]])
   )
 
+(import '[java.time LocalDate]
+        '[java.time.format DateTimeFormatter])
+
 (def dados_user (atom []))
-(def alimentos_armazenados (atom []))
-(def atividades_armazenadas (atom []))
+(def alimentos_user (atom []))
+(def atividades_user (atom []))
+(def datas_relatorio (atom []))
 
-(def api-key (env :usda-api-key))
+(def api_key_usda (env :usda-api-key))
+(def api_key_ninja (env :ninjas-api-key))
 
-(defn traduzir-alimento [texto de para]
+(defn traduzir_resultados [texto de para]
 
   (let [url "http://localhost:5000/translate"
 
@@ -32,14 +37,14 @@
   )
 
 
-(defn buscar-usda [alimento]
+(defn buscar_alimento [alimento]
 
   (let [url "https://api.nal.usda.gov/fdc/v1/foods/search"
 
         params {:query alimento
                 :dataType ["Survey (FNDDS)"]
                 :pageSize 10
-                :api_key api-key}
+                :api_key api_key_usda}
 
         response (http/get url {:query-params params :as :json})
 
@@ -51,7 +56,7 @@
                                    (:value %))
                                 (:foodNutrients item))
 
-                  descricao-pt (traduzir-alimento (:description item) "en" "pt")]
+                  descricao-pt (traduzir_resultados (:description item) "en" "pt")]
 
               {:descricao descricao-pt
                :energia-kcal energia}))
@@ -61,10 +66,52 @@
   )
 
 
+(defn buscar-resultados_alimento [alimento]
 
-(defn buscar-resultados [alimento]
+  (buscar_alimento (traduzir_resultados alimento "pt" "en"))
 
-  (buscar-usda (traduzir-alimento alimento "pt" "en"))
+  )
+
+(defn calorias_min [cal_hora]
+  (float (/ cal_hora 60))
+  )
+
+(defn buscar_atividade_fisica [atividade]
+
+  (let [url "https://api.api-ninjas.com/v1/caloriesburned"
+        query-params {"activity" atividade}]
+
+    (try
+
+      (let [response (http/get url
+                               {:headers {"X-Api-Key" api_key_ninja}
+                                :query-params query-params
+                                :as :json})
+
+            dados (:body response)]
+
+        (mapv (fn [item]
+
+                (let [descricao_pt (traduzir_resultados (:name item) "en" "pt")]
+
+                  {:descricao descricao_pt
+
+                   :energia-kcal (calorias_min (:calories_per_hour item))}))
+
+              dados))
+
+      (catch Exception e
+
+        (println "Erro ao buscar calorias:" (.getMessage e))
+
+        )
+      )
+    )
+  )
+
+(defn buscar_resultados_atividade_fisica [exercicio]
+
+  (buscar_atividade_fisica (traduzir_resultados exercicio "pt" "en"))
 
   )
 
@@ -72,20 +119,6 @@
 (defroutes app-routes
 
            (GET "/" [] "Nutri App by Arthur & Guilherme")
-
-           (POST "/traduzir" request
-
-             (let [params (json/decode (slurp (:body request)) true)
-                   texto (:texto params)
-                   de    (:de params)
-                   para  (:para params)]
-
-               {:status 200
-                :headers {"Content-Type" "application/json"}
-                :body (json/encode (traduzir-alimento texto de para))}
-
-               )
-             )
 
            (GET "/contem/user" []
 
@@ -102,7 +135,6 @@
                )
              )
 
-
            (POST "/registro/user" req
 
              (let [dados (json/decode (slurp (:body req)) true)]
@@ -116,10 +148,31 @@
                )
              )
 
-           (GET "/usda" [alimento]
+           (POST "/traduzir" request
+
+             (let [params (json/decode (slurp (:body request)) true)
+                   texto (:texto params)
+                   de    (:de params)
+                   para  (:para params)]
+
+               {:status 200
+
+                :headers {"Content-Type" "application/json"}
+
+                :body (json/encode (traduzir_resultados texto de para))}
+
+               )
+             )
+
+
+           (GET "/buscar/alimento" [alimento]
+
              {:status 200
+
               :headers {"Content-Type" "application/json"}
-              :body (json/encode (buscar-resultados alimento))}
+
+              :body (json/encode (buscar-resultados_alimento alimento))}
+
              )
 
 
@@ -127,20 +180,36 @@
 
              (let [dados (json/decode (slurp (:body req)) true)]
 
-               (swap! alimentos_armazenados conj dados)
+               (swap! alimentos_user conj dados)
 
                (println "Alimento recebido:" dados)
 
-               {:status 200 :body "Alimento registrado!"}
+               {:status 200
+
+                :body "Alimento registrado!"}
 
                )
              )
+
+           (GET "/buscar/atividade" [atividade]
+
+             (let []
+
+               {:status 200
+
+                :headers {"Content-Type" "application/json"}
+
+                :body (json/encode (buscar_resultados_atividade_fisica atividade))}
+
+               )
+             )
+
 
            (POST "/registro/atividade" req
 
              (let [dados (json/decode (slurp (:body req)) true)]
 
-               (swap! atividades_armazenadas conj dados)
+               (swap! atividades_user conj dados)
 
                (println "Atividade recebida:" dados)
 
@@ -156,11 +225,75 @@
 
               :headers {"Content-Type" "application/json"}
 
-              :body (json/encode {:alimentos @alimentos_armazenados
-                                  :atividades @atividades_armazenadas
+              :body (json/encode {:alimentos @alimentos_user
+                                  :atividades @atividades_user
                                   :dados @dados_user})}
 
              )
+
+
+           (POST "/datas/relatorio" req
+
+             (let [dados (json/decode (slurp (:body req)) true)]
+
+               (reset! datas_relatorio [dados])
+
+               (println "Datas do relatorio recebidas:" dados)
+
+               {:status 200
+                :body "Datas registradas!"}
+
+               )
+             )
+
+
+
+           (GET "/dados/user" []
+
+             {:status 200
+              :headers {"Content-Type" "application/json"}
+              :body (json/encode @dados_user)}
+
+             )
+
+           (GET "/dados/alimento" []
+             (let [formatter (DateTimeFormatter/ofPattern "dd/MM/yyyy")
+                   {:keys [inicio fim]} (first @datas_relatorio)
+                   inicio-ts (.toEpochDay (LocalDate/parse inicio formatter))
+                   fim-ts    (.toEpochDay (LocalDate/parse fim formatter))
+                   alimentos_filtrados (filter
+
+                                         (fn [{:keys [dataConsumo]}]
+
+                                           (let [data-ts (.toEpochDay (LocalDate/parse dataConsumo formatter))]
+
+                                             (<= inicio-ts data-ts fim-ts)))
+
+                                         @alimentos_user)]
+
+               {:status 200
+                :headers {"Content-Type" "application/json"}
+                :body (json/encode alimentos_filtrados)}))
+
+           (GET "/dados/ativ_fis" []
+             (let [formatter (DateTimeFormatter/ofPattern "dd/MM/yyyy")
+                   {:keys [inicio fim]} (first @datas_relatorio)
+                   inicio-ts (.toEpochDay (LocalDate/parse inicio formatter))
+                   fim-ts    (.toEpochDay (LocalDate/parse fim formatter))
+                   ativ_fis_filtrados (filter
+
+                                        (fn [{:keys [dataAtividade]}]
+
+                                           (let [data-ts (.toEpochDay (LocalDate/parse dataAtividade formatter))]
+
+                                             (<= inicio-ts data-ts fim-ts)))
+
+                                         @atividades_user)]
+
+               {:status 200
+                :headers {"Content-Type" "application/json"}
+                :body (json/encode ativ_fis_filtrados)}))
+
 
 
            (route/not-found "Not Found")
